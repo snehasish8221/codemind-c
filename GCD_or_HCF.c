e9e9fd07455db1b4b63603c28af846f4afa7039d
snehasish8221 <153939511+snehasish8221@users.noreply.github.com>
2023-12-30 20:45:41 +0530

    #include <stdio.h> int main() {     int num1, num2;     scanf("%d %d",&num1,&num2);     while (num2 != 0)     {         int temp = num2;         num2 = num1 % num2;         num1 = temp;     }     printf("%d",num1);     return 0; }

---
+++ GCD_or_HCF.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+    int num1, num2;
+    scanf("%d %d",&num1,&num2);
+    while (num2 != 0)
+    {
+        int temp = num2;
+        num2 = num1 % num2;
+        num1 = temp;
+    }
+    printf("%d",num1);
+    return 0;
+}
