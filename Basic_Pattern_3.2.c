bbe6c601d7ae31951bdd64c9b4ae6c2b68cb0770
snehasish8221 <153939511+snehasish8221@users.noreply.github.com>
2024-01-03 12:27:32 +0530

    #include<stdio.h> int main() {     int num;     scanf("%d",&num);     for(int i=1;i<=num;i++)     {         for(int j=1;j<=num+1-i;j++)         {             printf("%d ",i);         }         printf(" ");     } }

---
+++ Basic_Pattern_3.2.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int num;
+    scanf("%d",&num);
+    for(int i=1;i<=num;i++)
+    {
+        for(int j=1;j<=num+1-i;j++)
+        {
+            printf("%d ",i);
+        }
+        printf("
+");
+    }
+}
